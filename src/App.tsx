e110e56fca731dfb8023edd4d9dec898b8d6d4d9
Adam Mohammed <13.adamm@gmail.com>
2021-05-10 19:04:43 -0400

    added state and search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [recipesFound, setRecipesFound] = useState ([]);
+  const [recipeSearch, setRecipeSearch] = useState ('');
+
+  const searchForRecipes = async (query: string) => {
+    const result = await fetch(`http://localhost:3001/?search=${query}`)
+    return (await result.json()).results;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
